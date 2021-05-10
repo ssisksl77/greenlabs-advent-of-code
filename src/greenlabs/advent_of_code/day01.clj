a9ffc085b8076f0dc2ca8e6a18b0ac22412cd1ba
남영환 <ssisksl77@gmail.com>
2021-05-10 15:53:20 +0900

    Day 1 두번째 문제를 해결하라

--- src/greenlabs/advent_of_code/day01.clj
+++ src/greenlabs/advent_of_code/day01.clj
@@ -1,7 +1,25 @@
 (ns greenlabs.advent-of-code.day01 
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
-
-(-> (slurp (io/resource "day01.txt"))
+;; part one
+(->> (slurp (io/resource "day01.txt"))
     str/split-lines
-    (read-string))
+     (map read-string)
+     (apply +))
+
+;; part two
+(def input (->> "day01.txt"
+                io/resource
+                slurp
+                str/split-lines
+                (map read-string)))
+
+(loop [i (cycle input)
+       acc 0
+       s #{0}]
+  (if (seq i)
+    (let [res (+ acc (first i))]
+      (if (contains? s res)
+        res
+        (recur (next i) res (conj s res))))
+    nil))
