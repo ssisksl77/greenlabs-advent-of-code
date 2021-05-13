47b6cdf1255d061cd28e2604368747dd1a067fbb
남영환 <ssisksl77@gmail.com>
2021-05-13 14:39:32 +0900

    day04 풀이를 정리하라

--- src/greenlabs/advent_of_code/day04.clj
+++ src/greenlabs/advent_of_code/day04.clj
@@ -1,38 +1,34 @@
 (ns greenlabs.advent-of-code.day04
   (:require [clojure.string :as str]
-            [clojure.set :as set]
-            [greenlabs.advent-of-code.util :as util]
-            [clojure.java.io :as io]))
+            [greenlabs.advent-of-code.util :as util]))
 
 ; --- Day 4: Repose Record ---
-(def demo-input "[1518-11-01 00:00] Guard #10 begins shift
-[1518-11-01 00:05] falls asleep
-[1518-11-01 00:25] wakes up
-[1518-11-01 00:30] falls asleep
-[1518-11-01 00:55] wakes up
-[1518-11-01 23:58] Guard #99 begins shift
-[1518-11-02 00:40] falls asleep
-[1518-11-02 00:50] wakes up
-[1518-11-03 00:05] Guard #10 begins shift
-[1518-11-03 00:24] falls asleep
-[1518-11-03 00:29] wakes up
-[1518-11-04 00:02] Guard #99 begins shift
-[1518-11-04 00:36] falls asleep
-[1518-11-04 00:46] wakes up
-[1518-11-05 00:03] Guard #99 begins shift
-[1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up")
-
 "part one : 가장 많이 주무신 가드의 ID와 주무신 시간(분단위)를 곱하라"
 
-;; 1. 가드 단위로 partition을 나눌 수 있나?
-(def demo-guards (rest (str/split demo-input #" Guard ")))
+;; 1. 인풋 자르기
+(defn group-by-guard 
+  "가드단위로 자른 근무일지를 리턴합니다."
+  [input]
+  (letfn [(get-date [guard-shift-string]
+                    (->> (re-find #"\[(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d)\]" guard-shift-string)
+                         rest
+                         (apply str)))
+          (sort-input [input]
+                      (->> input 
+                           (sort-by #(Long/parseLong (get-date %)))
+                           (str/join "\n")))]
+    
+    (rest (str/split (sort-input input)
+                     #" Guard "))
+    ))
 
-;; 1. guard-shift-sheet를 만든다.
-(defn make-guards-shift-sheets [input]
+;; 2. 잠을 잔 범위 구하기
+(defn make-guards-shift-sheets 
+  " 가드별로 그룹핑한 숙면range 시트를 리턴합니다."
+  [input]
   (letfn [(make-sheet  [guard-shift]
             (let [[guard & sleeps] (str/split-lines guard-shift)
-                  id               (re-find #"\d+" guard) #_(subs guard 1 3)
+                  id               (re-find #"\d+" guard)
                   sleeps'          (partition 2 (map #(Long/parseLong (subs % 15 17)) sleeps))]
               [(keyword id) sleeps']))
           (grouping-guards [sheet]
@@ -42,11 +38,10 @@
          (map make-sheet)
          grouping-guards)))
 
-;; parsed data
-(make-guards-shift-sheets demo-guards)
-
-;; sleep-time
-(defn organize-sheet [guard-shift-sheets]
+;; 3. 가드가 얼마나 잤는지 계산한다.
+(defn organize-sheet 
+  "가드 시트지에서 가드가 얼마나 잤는지(:sleep-time)을 계산하여 리턴합니다."
+  [guard-shift-sheets]
   (let [sheet guard-shift-sheets]
     (map (fn [[id sleeps]]
            {:id     id
@@ -59,57 +54,64 @@
                                       sleeps))})
          sheet)))
 
-(defn find-sleepyhead-guard [organized-sheets]
+;; 4. 가장 많이 잔 가드를 찾는다.
+(defn find-sleepyhead-guard 
+  "가장 많이 잔 가드를 리턴합니다"
+  [organized-sheets]
   (reduce (fn [acc e] (if (< (:sleep-time acc) (:sleep-time e))
                         e
                         acc))
           {:sleep-time 0}
           organized-sheets))
 
-(defn dup-sleep-minute [guard]
+;; 5. 가장 자는시간이 겹친 minute을 계산한다.
+(defn dup-sleep-minute 
+  "가장 자는 시간이 겹친 minute을 리턴합니다"
+  [guard]
   (let [sleepyhead-guard      guard
         sleep-range           (map (fn [[start end]] (range start end)) (:sheet sleepyhead-guard))
         sleep-min-frequencies (frequencies (reduce (fn [acc e]
                                                      (apply conj acc e))
                                                    []
-                                                   sleep-range))]
-    (first  (reduce (fn [acc [min frequencies]]
-                      (if (< (second acc) frequencies)
-                        [min frequencies]
-                        acc))
-                    [0 0]
-                    sleep-min-frequencies))
-    ))
-
-(defn part-one [input]
-  (let [sleepyhead-guard (find-sleepyhead-guard (organize-sheet input))
-        best-minute (dup-sleep-minute sleepyhead-guard)]
+                                                   sleep-range))
+        min-max-fn (fn [acc [min frequencies]]
+                     (if (< (:fq acc) frequencies)
+                       {:min min :fq frequencies}
+                       acc))]
+    (assoc guard :most-sleep-min
+           (reduce min-max-fn
+                   {:min 0
+                    :fq 0}
+                   sleep-min-frequencies))))
+                   
+(defn part-one 
+  "가장 많이 잔 가드의 아이디와 가장 많이 잤던 분(minute)의 곱을 리턴합니다"
+  [guards-sheet]
+  (let [sleepyhead-guard (find-sleepyhead-guard (organize-sheet guards-sheet))
+        best-minute (:min (:most-sleep-min (dup-sleep-minute sleepyhead-guard)))]
 
     (* (Integer/parseInt (name (:id sleepyhead-guard))) best-minute)))
 
-(part-one (make-guards-shift-sheets (rest (str/split demo-input #" Guard "))))
-(part-one (make-guards-shift-sheets
-           (rest (str/split (slurp (io/resource "day04.txt"))
-                            #" Guard "))
-           ))
+#_(part-one (make-guards-shift-sheets (rest (str/split demo-input #" Guard "))))
+#_(part-one (make-guards-shift-sheets
+             (rest (str/split (slurp (io/resource "day04.txt"))
+                              #" Guard "))))
 
 
-(let [sorted-input (str/join "\n"
-                             (sort-by 
-                              #(Long/parseLong (apply str
-                                                (rest (re-find #"\[(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d)\]"                                                
-                                                               %))))
-                              
-                              #_#(subs % 1 17)
-                              (util/slurp-resource-lines "day04.txt")))
-      guards-sheet (make-guards-shift-sheets (rest (str/split sorted-input
-                                                              #" Guard ")))]
-  [(:id (find-sleepyhead-guard (organize-sheet guards-sheet ))) (reduce + (map (fn [[a b]]
-                    (let [n (- b a)]
-                      (if (pos? n)
-                        n
-                        (+ n 60))))
-                  (:sheet (find-sleepyhead-guard (organize-sheet guards-sheet )))))]
-  #_(dup-sleep-minute (find-sleepyhead-guard (organize-sheet guards-sheet)))
+(defn part-two
+  "잠을 잤던 분(minute)이 겹친 횟수가 가장 많았던 가드의 id와 분(minute)의 곱을 리턴합니다"
+  [guards-sheet]
+  (let [dup-sleep-minutes (map dup-sleep-minute (organize-sheet guards-sheet))
+        guard (reduce (fn [acc e]
+                        (if (< (:fq (:most-sleep-min acc)) (:fq (:most-sleep-min e)))
+                          e
+                          acc))
+                      dup-sleep-minutes)]
+
+    (* (Integer/parseInt (name (:id guard))) (:min (:most-sleep-min guard)))))
+
+
+(let [grouped-input (group-by-guard (util/slurp-resource-lines "day04.txt"))
+      guards-sheet (make-guards-shift-sheets grouped-input)]
   (part-one guards-sheet)
-  )
+  (part-two guards-sheet))
